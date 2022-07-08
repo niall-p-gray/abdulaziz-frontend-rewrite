562ce0007295444b91d4641fc7f62c47be11a800
Abdulaziz <39178255+abdu4188@users.noreply.github.com>
2022-07-08 11:25:33 +0300

    ✨ extract order table component

---
+++ components/orderPage/OrderTable.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-white border-b-2 border-gray-200 ">
+        <tr class="text-light">
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200 text-center">
+            ITEM
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            MON
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            TUE
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            WED
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            THU
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            FRI
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            SAT
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            SUN
+          </th>
+          <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
+            TOTAL
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index" class="bg-white border-b">
+          <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
+            <p class="font-semibold text-black">
+              {{ item.name }}
+            </p>
+            <div class="font-light">
+              <span v-for="(ingredient, inIndex) in item.ingredients" :key="`ing-${inIndex}`">
+                {{ ingredient }},
+              </span>
+            </div>
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+          <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
+            <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
+          </td>
+        </tr>
+        <tr class="bg-white border-b">
+          <td class=" px-4 py-2" colspan="7">
+            <span class="text-gray-400 font-light">
+              Last Edited: 7/7 12:17
+            </span>
+          </td>
+          <td class=" px-4 py-2 text-center">
+            <span class="text-gray-700 font-light text-lg">
+              Totals:
+            </span>
+          </td>
+          <td class=" px-4 py-2 text-center">
+            <span class="text-gray-900 font-semibold">
+              38
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrderTable',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/coffee-shop/order.vue
+++ pages/coffee-shop/order.vue
@@ -2,7 +2,7 @@
   <div class="bg-primary min-h-screen title-text">
     <div class="container !pt-36 !w-9/12">
       <h1 class="text-3xl title-text">
-        Howdy Kolache Links
+        Niall - FOR TESTING <span class="text-4xl">NPG</span>
       </h1>
       <div class="w-1/2 mt-10 grid grid-cols-2 text-center mx-auto gap-3">
         <div class="">
@@ -42,298 +42,7 @@
         </div>
       </div>
       <div class="mt-10">
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-white border-b-2 border-gray-200 ">
-              <tr class="text-light">
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200 text-center">
-                  ITEM
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  MON
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  TUE
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  WED
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  THU
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  FRI
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  SAT
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  SUN
-                </th>
-                <th scope="col" class="px-6 py-3 border-x-2 border-gray-200">
-                  TOTAL
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="bg-white border-b">
-                <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
-                  <p class="font-semibold text-black">
-                    Coloradough
-                  </p>
-                  <p class="font-light">
-                    Sausage, egg, cheddar, onions, peppers
-                  </p>
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-              </tr>
-              <tr class="bg-white border-b">
-                <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
-                  <p class="font-semibold text-black">
-                    Ron Swanson
-                  </p>
-                  <p class="font-light">
-                    Bacon, egg, jalapeño
-                  </p>
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-              </tr>
-              <tr class="bg-white border-b">
-                <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
-                  <p class="font-semibold text-black">
-                    El Greco
-                  </p>
-                  <p class="font-light">
-                    Spinach, egg, feta
-                  </p>
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-              </tr>
-              <tr class="bg-white border-b">
-                <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
-                  <p class="font-semibold text-black">
-                    Desayuno
-                  </p>
-                  <p class="font-light">
-                    Chorizo, egg, Chihuahua cheese
-                  </p>
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-              </tr>
-              <tr class="bg-white border-b">
-                <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
-                  <p class="font-semibold text-black">
-                    Hammy Sammy
-                  </p>
-                  <p class="font-light">
-                    Ham, Swiss cheese, stoneground mustard
-                  </p>
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-              </tr>
-              <tr class="bg-white border-b">
-                <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
-                  <p class="font-semibold text-black">
-                    K.R.E.A.M.
-                  </p>
-                  <p class="font-light">
-                    Cream cheese, lemon zest
-                  </p>
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-              </tr>
-              <tr class="bg-white border-b">
-                <td scope="row" class="px-4 py-2 border-x-2 border-gray-200 whitespace-nowrap">
-                  <p class="font-semibold text-black">
-                    The Schmear
-                  </p>
-                  <p class="font-light">
-                    Everything bagel & cream cheese
-                  </p>
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-                <td class="px-4 py-2 border-x-2 border-gray-200 text-center">
-                  <input type="text" class="border-2 border-gray-300 rounded-sm w-8 h-6">
-                </td>
-              </tr>
-              <tr class="bg-white border-b">
-                <td class=" px-4 py-2" colspan="7">
-                  <span class="text-gray-400 font-light">
-                    Last Edited: 7/7 12:17
-                  </span>
-                </td>
-                <td class=" px-4 py-2 text-center">
-                  <span class="text-gray-700 font-light text-lg">
-                    Totals:
-                  </span>
-                </td>
-                <td class=" px-4 py-2 text-center">
-                  <span class="text-gray-900 font-semibold">
-                    38
-                  </span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <order-table :items="items" />
       </div>
       <div class="mt-10 w-1/2 mx-auto">
         <div class="grid-grid-cols-2 gap-5">
@@ -372,7 +81,26 @@
 
 <script>
 export default {
-  name: 'CoffeeShopOrder'
+  name: 'CoffeeShopOrder',
+  components: {
+    OrderTable: () => import('@/components/orderPage/OrderTable')
+  },
+  data () {
+    return {
+      items: [
+        {
+          name: 'Coloradough',
+          ingredients: [
+            'Sausage',
+            'egg',
+            'cheddar',
+            'onions',
+            'peppers'
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
