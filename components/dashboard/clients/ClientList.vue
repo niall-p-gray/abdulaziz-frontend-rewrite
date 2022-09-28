8ae1d5a268fa867485953ade5495e19315006bb0
Yassin Youssoufm <yassinrsc@gmail.com>
2022-09-28 22:03:20 +0300

    Fix: Client list not loading

---
+++ components/dashboard/clients/ClientList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <GroupedClientTypeCard
+    v-for="(group, index) in groupedClientTypes"
+    :key="index"
+    :title="group.name"
+    :clients="group.clients"
+    :class="{'mt-6': index > 0}" />
+   </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import GroupedClientTypeCard from '@/components/dashboard/clients/GroupedClientTypeCard'
+
+export default {
+  components: {
+    GroupedClientTypeCard
+  },
+  computed: {
+    ...mapGetters({
+      clients: 'home/clients'
+    }),
+    groupedClientTypes () {
+      const groups = []
+
+      for (let index = 0; index < this.clients.length; index++) {
+        const client = this.clients[index]
+        const clientType = client.fields['Client Type'].replace(/^[0-9]\./, '')
+
+        if (client.fields['Show on TOC?'] === 0) {
+          continue
+        }
+
+        if (typeof client.fields.Active === 'undefined' || !client.fields.Active) {
+          continue
+        }
+
+        let group = groups.find(g => g.name === clientType)
+
+        if (!group) {
+          group = {
+            name: clientType,
+            clients: []
+          }
+
+          groups.push(group)
+        }
+
+        group.clients.push(client)
+
+        const groupIndex = groups.findIndex(g => g.name === clientType)
+        groups.splice(groupIndex, 1, group)
+      }
+
+      return groups.sort((a, b) => a.clients.length - b.clients.length)
+    }
+  }
+}
+</script>
---
+++ components/dashboard/clients/GroupedClientTypeCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <div class="card__title">
+      <h3 class="title-text">
+        {{ title }}
+      </h3>
+    </div>
+    <div v-if="clients.length > 0" class="card__content">
+      <nuxt-link v-for="(client, index) in clients" :key="index" :to="`clients/${client.fields['Rec ID']}`" class="card__content__link">
+        <span class="card__content__link__text"> {{ client.fields.Name }} </span>
+        <font-awesome-icon :icon="['fas', 'chevron-right']" class="card__content__link__icon" />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    clients: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
