9c52725f7c1e79b3afaf7cc3a8a55a04d0aa74df
Yassin Youssoufm <yassinrsc@gmail.com>
2022-09-30 21:06:31 +0300

    Adjust page heading font size on mobile

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -3,7 +3,7 @@
     <div>
       <BackButton v-if="shouldShowBackButton" />
       <div class="text-center mt-6 xl:mt-2">
-        <h1 class="text-4xl page-title font-medium">
+        <h1 class="text-3xl lg:text-4xl page-title font-medium">
           <portal-target name="page-title"></portal-target>
         </h1>
       </div>
