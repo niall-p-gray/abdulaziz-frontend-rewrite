5cba5a9e117edefa5c67d02251d40c30f46347be
Yassin Youssoufm <yassinrsc@gmail.com>
2022-10-04 12:25:49 +0300

    Add debug messages

--- pages/rewards/lookup.vue
+++ pages/rewards/lookup.vue
@@ -63,6 +63,8 @@ export default {
   },
   methods: {
     async search () {
+      console.log(process.env.TYPEFORM_API_KEY)
+      console.log(process.env.AIRTABLE_BASE_ID)
       if (typeof process.env.TYPEFORM_API_KEY === 'undefined') {
         console.log('TYPEFORM_API_KEY is not set')
       } else {
