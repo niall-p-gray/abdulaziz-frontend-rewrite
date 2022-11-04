4ec1504351c66387d22ef57b7d5613052e2d6e15
Yassin Youssoufm <yassinrsc@gmail.com>
2022-11-04 17:25:27 +0300

    Add item description popup on mobile

--- components/client-ordering-page/mobile/InlineProducDetails.vue
+++ components/client-ordering-page/mobile/InlineProducDetails.vue
@@ -3,7 +3,7 @@
     <div class="icon">
       <img v-if="product.logo" :src="product.logo" />
     </div>
-    <strong class="title">{{ product.name }}</strong>
+    <strong ref="title" class="title">{{ product.name }}</strong>
     <DailyOrderQuantityInput
       v-if="selectedday && product.weekDayOrders[selectedday]"
       :qty="product.weekDayOrders[selectedday].qty"
@@ -11,6 +11,7 @@
       :product="product"
       :show-buttons="true"
     />
+    <div v-if="showDescription" ref="description" class="description">{{ product.description }}</div>
   </div>
 </template>
 
@@ -28,18 +29,38 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      showDescription: false
+    }
+  },
   computed: {
     ...mapGetters({
       selectedWeek: 'weekly-client-orders/selectedWeek',
       selectedday: 'weekly-client-orders/selectedDay'
     })
+  },
+  methods: {
+    handleDescriptionVisibility (e) {
+      if (e.target === this.$refs.title || e.target === this.$refs.description) {
+        this.showDescription = true
+      } else {
+        this.showDescription = false
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('click', this.handleDescriptionVisibility)
+  },
+  beforeDestroy () {
+    window.removeEventListener('click', this.handleDescriptionVisibility)
   }
 }
 </script>
 
 <style scoped>
 .item {
-  @apply flex items-center justify-between py-3 border-y border-gray-300;
+  @apply flex items-center justify-between py-3 border-y border-gray-300 relative;
 }
 
 .icon {
@@ -48,5 +69,18 @@ export default {
 
 .title {
   @apply ml-2 underline flex-1;
+}
+
+.description{
+  position: absolute;
+  top: 45px;
+  left: 33px;
+  border-radius: 10px;
+  width: 275px;
+  height: 111px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  background: #fff;
+  padding: 10px;
+  z-index: 5;
 }
 </style>
