8ca7c33e031fd562a5757ab7c6099beba1834331
Yassin Youssoufm <yassinrsc@gmail.com>
2022-11-14 20:17:15 +0300

    Update “special notes” style

--- components/upcoming-orders/UpcomingOrder.vue
+++ components/upcoming-orders/UpcomingOrder.vue
@@ -1,57 +1,59 @@
 <template>
   <div class="order">
     <div @click="expanded = !expanded" class="main-content lg:cursor-pointer">
-      <div class="cell time lg:order-1">
-        <div class="title">Ready time</div>
-        <div class="value">{{ order.readyTime ? order.readyTime : '---' }}</div>
-      </div>
-      <div class="cell time lg:order-2">
-        <div class="title">Delivery time</div>
-        <div class="value">
-          {{ order.deliveryTime ? order.deliveryTime : '---' }}
+      <div class="flex flex-wrap">
+        <div class="cell time lg:order-1">
+          <div class="title">Ready time</div>
+          <div class="value">{{ order.readyTime ? order.readyTime : '---' }}</div>
         </div>
-      </div>
-      <div class="cell qty lg:order-3">
-        <div class="title">#</div>
-        <div class="value">{{ order.qty }}</div>
-      </div>
-      <div class="cell delivery lg:order-6">
-        <div class="title">Delivery</div>
-        <div class="value">
-          <div v-if="order.deliveryType">
-            <div v-if="order.deliveryType.toLowerCase() === 'delivery'" >
-              <img src="~/assets/icons/truck.svg" />
-              <div v-if="order.deliveryDriver" class="text-xs">{{ order.deliveryDriver }}</div>
-            </div>
-            <img
-              v-if="order.deliveryType.toLowerCase() === 'pickup'"
-              src="~/assets/icons/user-check.svg"
-            />
-          </div>
-          <div v-else>---</div>
-        </div>
-      </div>
-      <div class="cell client lg:order-7">
-        <div class="title">Client</div>
-        <div class="value">
-          <div class="flex justify-between items-center">
-            <div>
-              <p>{{ order.clientName }}</p>
-              <p v-if="order.clientDetails" class="text-sm font-normal">
-                {{ order.clientDetails }}
-              </p>
-            </div>
-            <div class="actions hidden lg:flex justify-end">
-              <a :href="order.editUrl" target="_blank">
-                <font-awesome-icon :icon="['fas', 'pencil']" class="text-xs"/>
-              </a>
-              <font-awesome-icon :icon="['fas', `chevron-${expanded ? 'up' : 'down'}`]" class="text-s ml-4" />
-            </div>
+        <div class="cell time lg:order-2">
+          <div class="title">Delivery time</div>
+          <div class="value">
+            {{ order.deliveryTime ? order.deliveryTime : '---' }}
           </div>
         </div>
+        <div class="cell qty lg:order-3">
+          <div class="title">#</div>
+          <div class="value">{{ order.qty }}</div>
+        </div>
+        <div class="cell delivery lg:order-6">
+          <div class="title">Delivery</div>
+          <div class="value">
+            <div v-if="order.deliveryType">
+              <div v-if="order.deliveryType.toLowerCase() === 'delivery'" >
+                <img src="~/assets/icons/truck.svg" />
+                <div v-if="order.deliveryDriver" class="text-xs">{{ order.deliveryDriver }}</div>
+              </div>
+              <img
+                v-if="order.deliveryType.toLowerCase() === 'pickup'"
+                src="~/assets/icons/user-check.svg"
+              />
+            </div>
+            <div v-else>---</div>
+          </div>
+        </div>
+        <div class="cell client lg:order-7">
+          <div class="title">Client</div>
+          <div class="value">
+            <div class="flex justify-between items-center">
+              <div>
+                <p>{{ order.clientName }}</p>
+                <p v-if="order.clientDetails" class="text-sm font-normal">
+                  {{ order.clientDetails }}
+                </p>
+              </div>
+              <div class="actions hidden lg:flex justify-end">
+                <a :href="order.editUrl" target="_blank">
+                  <font-awesome-icon :icon="['fas', 'pencil']" class="text-xs"/>
+                </a>
+                <font-awesome-icon :icon="['fas', `chevron-${expanded ? 'up' : 'down'}`]" class="text-s ml-4" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <div v-if="order.notes" class="special-notes"><strong>Special Notes: </strong>{{ order.notes }}</div>
     </div>
-    <div v-if="order.notes" class="text-sm text-gray-500 pl-5 pt-1">{{ order.notes }}</div>
     <div :class="{expanded: expanded}" class="expandable-content">
       <div class="details">
         <div class="detail">
